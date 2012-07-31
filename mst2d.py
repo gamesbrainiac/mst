b346b60c883189f042672fc1e88a16537e6b6770
Huang Kan <huangkandiy@gmail.com>
2012-07-31 14:59:09 -0400

    add optimal pair prime function and ratio

--- mst2d.py
+++ mst2d.py
@@ -12,7 +12,7 @@ class point:
         self.x = x
         self.y = y
     def distance2(self, p):
-        return (self.x - p.x) ** 2 + (self.y - p.y) ** 2
+        return ((self.x - p.x) ** 2 + (self.y - p.y) ** 2) ** 0.5
     def plot(self, marker):
         pp.plot(self.x, self.y, marker)
     def pair_dis2(self, pr):
@@ -45,23 +45,23 @@ def point_generator(n = 10, bound = 5.0):
         plist.append(point(rd.ranf() * bound, rd.ranf() * bound))
     return plist
 
-def prim(plist):
+def prim(pl):
     connected = [0]
-    noconnected = list(range(1, len(plist)))
+    dis = list(range(1, len(pl)))
     edges = []
     sum = 0
-    while len(connected) < len(plist):
-        edge = 0,noconnected[0]
-        shortest = plist[0].distance2(plist[noconnected[0]])
+    while len(connected) < len(pl):
+        shortest = float('inf')
         for i in connected:
-            for j in noconnected:
-                l = plist[i].distance2(plist[j])
+            for j in dis:
+                l = pl[i].distance2(pl[j])
                 if l < shortest:
                     shortest = l
-                    edge = i, j
+                    edge = pl[i],pl[j]
+                    idx = i,j
         sum += shortest
-        connected.append(edge[1])
-        noconnected.remove(edge[1])
+        connected.append(idx[1])
+        dis.remove(idx[1])
         edges.append(edge)
     return edges, sum
 
@@ -70,14 +70,14 @@ def draw_prim(pl, edges):
     #pp.subplot(1,3,1)
     for point in pl:
         point.plot('ro')
-    for x,y in edges:
-        xs = [pl[x].x, pl[y].x]
-        ys = [pl[x].y, pl[y].y]
+    for p,q in edges:
+        xs = [p.x, q.x]
+        ys = [p.y, q.y]
         pp.plot(xs, ys, 'y-', alpha = 1)
     pp.show()
 
 
-def pair_generator(n = 10, bound = 5.0):
+def pair_generator(n = 10, bound = 10.0):
     pl = point_generator(n, bound)
     pairlist = [pair(p, point((p.x + 1.0), p.y)) for p in pl]
     return pairlist
@@ -116,7 +116,7 @@ def pair_prim(pl):
     return edges, sum
 
 
-def draw_pair_prim(prl, edges):
+def draw_pair_prim(prl, edges, opt_e):
     pp.figure()
     for pr in prl:
         pr.plot('ro', 'bx')
@@ -125,20 +125,51 @@ def draw_pair_prim(prl, edges):
         xs = [p.x, q.x]
         ys = [p.y, q.y]
         pp.plot(xs, ys, 'y-', alpha = 1)
+    for e in opt_e:
+        p, q = e
+        xs = [p.x, q.x]
+        ys = [p.y, q.y]
+        pp.plot(xs, ys, 'b-', alpha = 1)       
     pp.show()
 
-
-        
+def brute_opt_prim(prl):
+    opt_sum = float('inf')
+    for i in range(2 ** len(prl) - 1):
+        key = str(bin(i))[2:]
+        key = '0'*(len(prl) - len(key)) + key
+        pl = []
+        for j in range(len(prl)):
+            pl.append(prl[j].ps[int(key[j])])
+        edges, sum = prim(pl)
+        if sum < opt_sum:
+            opt_edges, opt_sum = edges, sum
+    return opt_edges, opt_sum
+                        
 def main_prim():
     pl = point_generator(20)
     edges, sum = prim(pl)
     draw_prim(pl, edges)
     
-def main_pair():
-    prl = pair_generator()
+def main_pair(np):
+    prl = pair_generator(np)
     edges, sum = pair_prim(prl)
-    draw_pair_prim(prl, edges)
+    opt_edges, sum2 = brute_opt_prim(prl)
+    draw_pair_prim(prl, edges, opt_edges)
     
+#def main_opt(n):
+#    prl = pair_generator(n)
+#    edges, sum1 = pair_prim(prl)
+#    opt_edges, sum2 = brute_opt_prim(prl)
+#    return sum1/sum2
+#def opt_test(n, np):
+#    worst = 0
+#    for i in range(n):
+#        ratio = main_opt(np)
+#        if ratio > worst:
+#            worst = ratio
+#    print(worst)
+
+main_pair(15)
     
-main_pair()
+ 
         
